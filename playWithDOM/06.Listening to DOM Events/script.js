069c421823dcf6ee2fc60880f7e38142f639db1f
jahid-sourav <info.jahidsourav@gmail.com>
2022-11-16 09:31:14 +0600

    06 End

---
+++ playWithDOM/06.Listening to DOM Events/script.js
@@ -0,0 +1,136 @@
+// Event Listeners
+const headerElement = document.querySelector('#header');
+const inputElement = document.querySelector('input[type ="text"]');
+const formElement = document.querySelector('form');
+/**********************************************************/
+// Single Click Event
+// headerElement.addEventListener('click', (event) => {
+//     console.log(event);
+// });
+/**********************************************************/
+
+/**********************************************************/
+// Double Click Event
+// headerElement.addEventListener('dblclick', (event) => {
+//     console.log(event);
+// });
+/**********************************************************/
+
+/**********************************************************/
+// Mouse Down Event
+// headerElement.addEventListener('mousedown', (event) => {
+//     console.log(event);
+// });
+/**********************************************************/
+
+/**********************************************************/
+// Mouse UP Event
+// headerElement.addEventListener('mouseup', (event) => {
+//     console.log(event);
+// });
+/**********************************************************/
+
+/**********************************************************/
+// Mouse Enter Event
+// headerElement.addEventListener('mouseenter', (event) => {
+//     console.log(event);
+// });
+/**********************************************************/
+
+/**********************************************************/
+// Mouse Leave Event
+// headerElement.addEventListener('mouseleave', (event) => {
+//     console.log(event);
+// });
+/**********************************************************/
+
+/**********************************************************/
+// Mouse Over Event
+// headerElement.addEventListener('mouseover', (event) => {
+//     console.log(event);
+// });
+/**********************************************************/
+
+/**********************************************************/
+// Mouse Out Event
+// headerElement.addEventListener('mouseout', (event) => {
+//     console.log(event);
+// });
+/**********************************************************/
+
+/**********************************************************/
+// Key Down Event
+// inputElement.addEventListener('keydown', (event) => {
+//     console.log(event);
+// });
+/**********************************************************/
+
+/**********************************************************/
+// Key UP Event
+// inputElement.addEventListener('keyup', (event) => {
+//     console.log(event);
+// });
+/**********************************************************/
+
+/**********************************************************/
+// Key Press Event
+// inputElement.addEventListener('keypress', (event) => {
+//     console.log(event);
+// });
+/**********************************************************/
+
+/**********************************************************/
+// Focus Event
+// inputElement.addEventListener('focus', (event) => {
+//     console.log(event);
+// });
+/**********************************************************/
+
+/**********************************************************/
+// Blur Event
+// inputElement.addEventListener('blur', (event) => {
+//     console.log(event);
+// });
+/**********************************************************/
+
+/**********************************************************/
+// Cut Event
+// inputElement.addEventListener('cut', (event) => {
+//     console.log(event);
+// });
+/**********************************************************/
+
+/**********************************************************/
+// Paste Event
+// inputElement.addEventListener('paste', (event) => {
+//     alert('Do you want to paste?');
+// });
+/**********************************************************/
+
+/**********************************************************/
+// Input Event
+// inputElement.addEventListener('input', (event) => {
+//     console.log(event);
+// });
+/**********************************************************/
+
+/**********************************************************/
+// Form Event
+// formElement.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     // console.log(event);
+//     console.log(event.target);
+// });
+/**********************************************************/
+
+/**********************************************************/
+// Input Value Event
+// inputElement.addEventListener('keydown', (event) => {
+//     console.log(event.target.value);
+// });
+/**********************************************************/
+
+/**********************************************************/
+// https://www.w3schools.com/js/js_htmldom_events.asp
+// https://www.w3schools.com/jsref/dom_obj_event.asp
+/**********************************************************/
